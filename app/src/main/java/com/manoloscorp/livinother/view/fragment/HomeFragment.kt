a2fecb584964d5dfeeee1bb44d9c9d2056dd4b17
brect <b.limas93@gmail.com>
2020-10-22 22:20:24 -0300

    ajuste tela perfil. criado tela home

--- app/src/main/java/com/manoloscorp/livinother/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/manoloscorp/livinother/view/fragment/HomeFragment.kt
@@ -28,10 +28,6 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
 
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-
         return root
     }
 }
